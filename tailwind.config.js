e695dd886579daff70f68beb9baa7309ee9047fd
Abighail Shafira Ihsani <abighailshafira@upi.edu>
2022-10-14 11:30:38 +0700

    Edit configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", 
-  "./component/*.{html,js}"],
+  content: ["index.html", "./component/*.{html,js}"],
   theme: {
     container: {
       center: true,
-      padding: "30px",
+      padding: "25px",
     },
-    extend: {},
+    extend: {
+      colors: {
+        primary: "",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
   },
   plugins: [],
 };
